8b7626a7d701aeaa88db7db2cf3e0e44b14deb51
Michael Dunn <dunnmj42@gmail.com>
2021-01-15 16:04:19 -0600

    axios GET gallery

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [imageList, setImageList] = useState([]);
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  const getImages = () => {
+    axios
+      .get("/gallery")
+      .then((response) => {
+        setImageList(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {imageList.map((image) => (
+          <img key={image.id} src={image.path}/>
+        ))}
       </div>
     );
 }
